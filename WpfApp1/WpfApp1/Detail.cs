3b066f126ce3d66fc02c20421cd9905e39dcc652
kei ito <ito.kei@jvckenwood.com>
2024-05-22 09:48:56 +0900

    first commit

---
+++ WpfApp1/WpfApp1/Detail.cs
@@ -0,0 +1,33 @@
+﻿using Reactive.Bindings;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Detail
+    {
+        public ReactivePropertySlim<bool> IsEnabled { get; } = new(true);
+
+        public ReactivePropertySlim<bool> IsReadOnly { get; } = new(false);
+
+        public ReactivePropertySlim<int> Number { get; } = new(1);
+
+        public ReactivePropertySlim<string> Text1 { get; } = new(string.Empty);
+
+        public ReactivePropertySlim<string> Text2 { get; } = new(string.Empty);
+
+        public ReactivePropertySlim<string> Text3 { get; } = new(string.Empty);
+
+        public Detail(int number, string text1, string text2, string text3)
+        {
+            Number.Value = number;
+            Text1.Value = text1;
+            Text2.Value = text2;
+            Text3.Value = text3;
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Reactive.Bindings;
+
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ReactiveCollection<Detail> Details { get; } = [];
+
+        public MainWindow()
+        {
+            InitDetails();
+
+            InitializeComponent();
+        }
+
+        private void InitDetails()
+        {
+            for(int i=0; i<10; i++)
+            {
+                Details.Add(new(i + 1, $"item {i + 1}", $"item {i + 1}", $"item {i + 1}"));
+            }
+        }
+    }
+}
